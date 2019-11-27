ae99e1af4c82634b62609522dcd3ba23b8855ff4
MAURITS DOES <2111633@talnet.nl>
2019-11-27 14:17:27 +0100

    absent/present scherm toegevoegd.(niet af). styling van buttons toegevoegd

---
+++ absent.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Absent/Present</title>
+</head>
+<body>
+
+    <div class ="content">
+        <div>
+            <h1>Absent of Present?</h1>
+        </div>
+        <button class="absent">Absent</button>
+        <button class="present">Present</button>
+    </div>
+</body>
+</html>
