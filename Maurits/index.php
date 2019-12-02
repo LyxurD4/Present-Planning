1797fc639da775512689485821cf07a67848c7cb
MAURITS DOES <2111633@talnet.nl>
2019-12-02 08:59:03 +0100

    voor handigheid

---
+++ Maurits/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <title>Absent/Present</title>
+</head>
+<body>
+    <div class ="content">
+        <div>
+            <h1>Ben je present of absent?</h1>
+        </div>
+        <div class="buttons">
+            <form action="verwijs.php" method="post">
+                <input type="submit" value="Present" class="present" name="present">
+                <input type="submit" value="Absent" class="absent" name="absent">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
